0df5ee89c7fe6519d26600bcd7701f243c621ee4
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-11-20 22:16:03 +0200

    refactor: replace images.domains with images.remotePatterns in next.config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    // this is technically deprecated but works perfectly.
-    // if you'd like to see the remotePatterns version, I'm happy to merge a PR
-    domains: [
-      'localhost',
-      'digitalhippo-production.up.railway.app',
-    ],
-  },
-}
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+			},
+			{
+				protocol: "https",
+				hostname: "digitalhippo-production.up.railway.app",
+			},
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
