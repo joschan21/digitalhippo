bc4a24fb630113b8642421186cf86ffa605ea3e1
Emilio <87607608+Made-in-Heaven-dotcom@users.noreply.github.com>
2023-11-30 21:20:36 -0400

    Verify if the product is already in the cart to prevent that the same item is added multiple times.

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -21,6 +21,21 @@ const AddToCartButton = ({
     return () => clearTimeout(timeout)
   }, [isSuccess])
 
+  const handleAddToCart = () => {
+    //Verify if the item is in the cart
+    const isProductInCart = items.some((item) => item.product.id === product.id);
+
+    if (!isProductInCart) {
+      //If not, the item can be added. Also show a toast (I personally used Toast from Shadcn).
+      addItem(product);
+      setIsSuccess(true);
+      toast({ variant: 'success', title: '¡Artículo añadido al carrito!' });
+    } else {
+      //If the item is already in the cart, then it won't be added. Also show a toast.
+      toast({ variant: 'destructive', title: '¡Este artículo ya está en el carrito!' });
+    }
+  };
+
   return (
     <Button
       onClick={() => {
