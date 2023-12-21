2e3053f071a6384839cb94ec3691b6bfabaf7e82
Emilio <87607608+corazon-roto@users.noreply.github.com>
2023-12-21 09:19:30 -0400

    back to back

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,24 +1,14 @@
-import { appRouter } from '@/trpc';
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import { NextApiResponse } from 'next';
+import { appRouter } from '@/trpc'
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
 
-const handler = async (req: Request, res: NextApiResponse) => {
-  try {
-    const result = await fetchRequestHandler({
-      endpoint: '/api/trpc',
-      req,
-      router: appRouter,
-      // @ts-expect-error context already passed from express middleware
-      createContext: () => ({}),
-    });
+const handler = (req: Request) => {
+  fetchRequestHandler({
+    endpoint: '/api/trpc',
+    req,
+    router: appRouter,
+    // @ts-expect-error context already passed from express middleware
+    createContext: () => ({}),
+  })
+}
 
-    // Devuelve la respuesta
-    res.status(200).json(result);
-  } catch (error) {
-    // Maneja cualquier error
-    console.error('Error en el manejador de la ruta:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
-
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -1,11 +1,11 @@
-import { User } from '../payload-types';
-import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
-import { NextRequest } from 'next/server';
+import { User } from '../payload-types'
+import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
+import { NextRequest } from 'next/server'
 
 export const getServerSideUser = async (
   cookies: NextRequest['cookies'] | ReadonlyRequestCookies
 ) => {
-  const token = cookies.get('payload-token')?.value;
+  const token = cookies.get('payload-token')?.value
 
   const meRes = await fetch(
     `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
@@ -14,15 +14,11 @@ export const getServerSideUser = async (
         Authorization: `JWT ${token}`,
       },
     }
-  );
+  )
 
-  const responseText = await meRes.text();
-
-  try {
-    const { user } = JSON.parse(responseText);
-    return { user };
-  } catch (error) {
-    console.error('Error al analizar la respuesta JSON:', error);
-    return { user: null }; // o manejar el error de acuerdo a tus necesidades
+  const { user } = (await meRes.json()) as {
+    user: User | null
   }
-};
+
+  return { user }
+}
