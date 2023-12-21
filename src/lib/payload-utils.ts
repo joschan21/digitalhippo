ef0ceb909569b2f3271af424eb15632532403c24
Emilio <87607608+corazon-roto@users.noreply.github.com>
2023-12-20 22:18:38 -0400

    attemp #4

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -1,11 +1,11 @@
-import { User } from '../payload-types'
-import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
-import { NextRequest } from 'next/server'
+import { User } from '../payload-types';
+import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
+import { NextRequest } from 'next/server';
 
 export const getServerSideUser = async (
   cookies: NextRequest['cookies'] | ReadonlyRequestCookies
 ) => {
-  const token = cookies.get('payload-token')?.value
+  const token = cookies.get('payload-token')?.value;
 
   const meRes = await fetch(
     `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
@@ -16,10 +16,14 @@ export const getServerSideUser = async (
     }
   );
 
-  
-  const { user } = await meRes.json();
-  console.log(meRes);
+  const responseText = await meRes.text();
+  console.log(responseText); // Imprime el contenido de la respuesta
 
-
-  return { user }
-}
+  try {
+    const { user } = JSON.parse(responseText);
+    return { user };
+  } catch (error) {
+    console.error('Error al analizar la respuesta JSON:', error);
+    return { user: null }; // o manejar el error de acuerdo a tus necesidades
+  }
+};
