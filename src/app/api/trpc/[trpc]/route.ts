11b4c17ec3b959addc1796c532887b0794e38085
Emilio <87607608+corazon-roto@users.noreply.github.com>
2023-12-24 14:17:29 -0400

    attempt #11

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,25 +1,14 @@
-import { appRouter } from '@/trpc';
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
+import { appRouter } from '@/trpc'
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
 
-const handler = async (req: Request) => {
-  try {
-    const result = await fetchRequestHandler({
-      endpoint: '/api/trpc',
-      req,
-      router: appRouter,
-      // @ts-expect-error context already passed from express middleware
-      createContext: () => ({}),
-    });
+const handler = (req: Request) => {
+  fetchRequestHandler({
+    endpoint: '/api/trpc',
+    req,
+    router: appRouter,
+    // @ts-expect-error context already passed from express middleware
+    createContext: () => ({}),
+  })
+}
 
-    // Devuelve la respuesta obtenida
-    return new Response(JSON.stringify(result), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  } catch (error) {
-    // Maneja cualquier error y devuelve una respuesta de error apropiada
-    console.error('Error handling request:', error);
-    return new Response('Internal Server Error', { status: 500 });
-  }
-};
-
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
